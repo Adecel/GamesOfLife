5b075ed4b344e5e146d282dba8386f9735c2a1d3
adecel <rustymabiala150@gmail.com>
2024-01-23 13:57:04 +0200

    For the record and commits (23/Jan/2024 - 13:47) 1-the rest of file concerning the pass commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function destroy(Request $request){
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    } // End of destroy
+}
